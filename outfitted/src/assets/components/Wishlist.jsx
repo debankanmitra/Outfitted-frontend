ec2f7aa4093606196f93ef2f453c1c2a8cd468c5
debankanmitra <debankanmitra@gmail.com>
2024-02-21 08:42:44 +0530

    fix motion of the wishlist

--- outfitted/src/assets/components/Wishlist.jsx
+++ outfitted/src/assets/components/Wishlist.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
+import PropTypes from 'prop-types';
+
 
 const products = [
   {
@@ -27,8 +29,13 @@ const products = [
   // More products...
 ]
 
-export default function Wishlist() {
+export default function Wishlist({onClose}) {
   const [open, setOpen] = useState(true)
+  async function handleClick() {
+    setOpen(false);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    onClose();
+  }
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -61,12 +68,12 @@ export default function Wishlist() {
                   <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                        <Dialog.Title className="text-lg font-medium text-gray-900">Wishlist</Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
                           <button
                             type="button"
                             className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
-                            onClick={() => setOpen(false)}
+                            onClick={handleClick}
                           >
                             <span className="absolute -inset-0.5" />
                             <span className="sr-only">Close panel</span>
@@ -129,7 +136,7 @@ export default function Wishlist() {
                           href="/"
                           className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
-                          Wishlist
+                          Move to Cart
                         </a>
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
@@ -140,8 +147,7 @@ export default function Wishlist() {
                             className="font-medium text-indigo-600 hover:text-indigo-500"
                             onClick={() => setOpen(false)}
                           >
-                            Continue Shopping
-                            <span aria-hidden="true"> &rarr;</span>
+                            Continue Shopping<span aria-hidden="true"> &rarr;</span>
                           </button>
                         </p>
                       </div>
@@ -156,3 +162,7 @@ export default function Wishlist() {
     </Transition.Root>
   )
 }
+
+Wishlist.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
